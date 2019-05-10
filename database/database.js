8d1d8965bff1a36bec79577a8469b73cbbc246b2
Paola <gborja311@gmail.com>
2019-05-10 09:46:55 -0500

    prue

---
+++ database/database.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+    'urlMySql':{
+       host: "localhost",
+       user: "root",
+       password: "",
+       database: "arbol"
+    },
+    //Por favor, reemplace su archivo de host aquí: 127.1.1.0 , Express is Collection Name (Database Name)
+};
